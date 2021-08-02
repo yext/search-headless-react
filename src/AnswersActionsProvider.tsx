3d62bead55e6d4290154856b3b8cd8b724a6be75
Oliver Shi <oshi@yext.com>
2021-08-02 15:05:12 -0400

    dont set vertical key in provider

--- src/AnswersActionsProvider.tsx
+++ src/AnswersActionsProvider.tsx
@@ -4,16 +4,12 @@ import { AnswersConfig } from '@yext/answers-core';
 import { AnswersActions, AnswersActionsContext } from './AnswersActionsContext';
 
 interface Props extends AnswersConfig {
-  verticalKey?: string
   children?: ReactChildren | ReactChild | (ReactChildren | ReactChild)[]
 }
 
 export function AnswersActionsProvider(props: Props): JSX.Element {
-  const { children, verticalKey, ...answerConfig } = props;
+  const { children, ...answerConfig } = props;
   const storeActions: AnswersActions = provideStatefulCore(answerConfig);
-  if (verticalKey) {
-    storeActions.setVerticalKey(verticalKey);
-  }
   return (
     <AnswersActionsContext.Provider value={storeActions}>
       {children}
