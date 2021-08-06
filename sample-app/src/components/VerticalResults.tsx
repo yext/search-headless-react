e716073f617529a1fbd5a1a7af59d811a222a3e9
Tom Meyer <tmeyer@yext.com>
2021-08-06 10:20:36 -0400

    Add basic `VerticalResults` and `StandardCard` components.  (#2)

---
+++ sample-app/src/components/VerticalResults.tsx
@@ -0,0 +1,42 @@
+import { Result } from '@yext/answers-core';
+import { subscribeToStateUpdates } from '@yext/answers-headless-react';
+import { CardComponent, CardConfigTypes } from '../models/cardComponent';
+
+interface Props {
+  CardComponent: CardComponent,
+  cardConfig: CardConfigTypes,
+  results?: Result[]
+}
+
+/**
+ * A Component that displays all the search results for a given vertical.
+ * 
+ * @param props - The props for the Component, including the results and the card type
+ *                to be used.
+ */
+function VerticalResults(props: Props): JSX.Element {
+  const { results, CardComponent, cardConfig } = props;
+
+  return (
+    <section className='yxt-Results'>
+      <div className='yxt-Results-items'>
+        {results && results.map(result => renderResult(CardComponent, cardConfig, result))}
+      </div>
+    </section>
+  )
+}
+
+/**
+ * Renders a single result using the specified card type and configuration.
+ * 
+ * @param CardComponent - The card for the vertical.
+ * @param cardConfig - Any card-specific configuration.
+ * @param result - The result to render.
+ */
+function renderResult(CardComponent: CardComponent, cardConfig: CardConfigTypes, result: Result): JSX.Element {
+  return <CardComponent result={result} configuration={cardConfig} key={result.id}/>;
+}
+
+export default subscribeToStateUpdates(state => {
+  return { results: state.vertical.results?.verticalResults.results };
+})(VerticalResults);
