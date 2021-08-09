6d4ac36f5a9ce5443d1618515d87f29b40c6ab4e
Oliver Shi <oshi@yext.com>
2021-08-09 19:27:26 -0400

    respond to comments

--- sample-app/src/components/Autocomplete.tsx
+++ sample-app/src/components/Autocomplete.tsx
@@ -1,6 +1,8 @@
 import { AutocompleteResult } from '@yext/answers-core';
 import { MutableRefObject, useEffect, useState } from 'react';
 import '../sass/Autocomplete.scss';
+import classNames from 'classnames';
+import renderWithHighlighting from './utils/renderWithHighlighting';
 
 interface Props {
   autocompleteResults: AutocompleteResult[]
@@ -12,43 +14,47 @@ interface Props {
 export default function Autocomplete({ autocompleteResults, inputRef, onSelect, onOptionClick }: Props) {
   const [selectedIndex, setSelectedIndex] = useState<number>(-1);
   const [shouldDisplay, setShouldDisplay] = useState<boolean>(true);
+
+  function handleKeyDown(evt: KeyboardEvent) {
+    if (['ArrowDown', 'ArrowUp'].includes(evt.key)) {
+      evt.preventDefault();
+    }
+
+    if (evt.key === 'Enter') {
+      setShouldDisplay(false);
+      setSelectedIndex(-1);
+    } else if (evt.key === 'Escape') {
+      setSelectedIndex(-1);
+      setShouldDisplay(false);
+    } else if (evt.key === 'ArrowDown' && selectedIndex < autocompleteResults.length - 1) {
+      onSelect && onSelect(autocompleteResults[selectedIndex + 1]?.value || '');
+      setSelectedIndex(selectedIndex + 1);
+      setShouldDisplay(true);
+    } else if (evt.key === 'ArrowUp' && selectedIndex >= 0) {
+      onSelect && onSelect(autocompleteResults[selectedIndex - 1]?.value || '');
+      setSelectedIndex(selectedIndex - 1);
+      setShouldDisplay(true);
+    }
+  }
+
+  function toggleDisplayOn() {
+    setShouldDisplay(true);
+  }
+
+  function handleDocumentClick(evt: MouseEvent) {
+    const target = evt.target as HTMLElement;
+    if (!target || !target.isSameNode(inputRef.current)) {
+      setSelectedIndex(-1);
+      setShouldDisplay(false);
+    } else {
+      setShouldDisplay(true);
+    }
+  }
+
   useEffect(() => {
     const currentRef = inputRef?.current
     if (!currentRef) {
       return;
-    }
-    function handleKeyDown(evt: KeyboardEvent) {
-      if (['ArrowDown', 'ArrowUp'].includes(evt.key)) {
-        evt.preventDefault();
-      }
-
-      if (evt.key === 'Enter') {
-        setShouldDisplay(false);
-        setSelectedIndex(-1);
-      } else if (evt.key === 'Escape') {
-        setSelectedIndex(-1);
-        setShouldDisplay(false);
-      } else if (evt.key === 'ArrowDown' && selectedIndex < autocompleteResults.length - 1) {
-        onSelect && onSelect(autocompleteResults[selectedIndex + 1]?.value || '');
-        setSelectedIndex(selectedIndex + 1);
-        setShouldDisplay(true);
-      } else if (evt.key === 'ArrowUp' && selectedIndex >= 0) {
-        onSelect && onSelect(autocompleteResults[selectedIndex - 1]?.value || '');
-        setSelectedIndex(selectedIndex - 1);
-        setShouldDisplay(true);
-      }
-    }
-    function toggleDisplayOn() {
-      setShouldDisplay(true);
-    }
-    function handleDocumentClick(evt: MouseEvent) {
-      const target = evt.target as HTMLElement;
-      if (!target || !target.isSameNode(inputRef.current)) {
-        setSelectedIndex(-1);
-        setShouldDisplay(false);
-      } else {
-        setShouldDisplay(true);
-      }
     }
     currentRef.addEventListener('keydown', handleKeyDown);
     currentRef.addEventListener('change', toggleDisplayOn);
@@ -68,9 +74,9 @@ export default function Autocomplete({ autocompleteResults, inputRef, onSelect, 
   return (
     <div className='Autocomplete'>
       {autocompleteResults.map((result, index) => {
-        const className = index === selectedIndex
-          ? 'Autocomplete-option Autocomplete-option--selected'
-          : 'Autocomplete-option'
+        const className = classNames('Autocomplete-option', {
+          'Autocomplete-option--selected': index === selectedIndex
+        });
         return (
           <div key={result.value} className={className} onClick={() => {
             setSelectedIndex(index);
@@ -85,23 +91,4 @@ export default function Autocomplete({ autocompleteResults, inputRef, onSelect, 
   )
 }
 
-const renderWithHighlighting = ({ value, matchedSubstrings }: AutocompleteResult) => {
-  if (!matchedSubstrings || matchedSubstrings.length === 0) {
-    return <span>{value}</span>;
-  }
-  const substrings = [...matchedSubstrings];
-  substrings.sort((a, b) => a.offset - b.offset);
-  const highlightedJSX = []
-  let curr = 0;
-  for (let { offset, length } of substrings) {
-    if (offset > curr) {
-      highlightedJSX.push(<span key={curr}>{value.substring(curr, offset)}</span>)
-    }
-    highlightedJSX.push(<strong key={offset}>{value.substring(offset, offset + length)}</strong>)
-    curr = offset + length;
-  }
-  if (curr < value.length) {
-    highlightedJSX.push(<span key={curr}>{value.substring(curr)}</span>)
-  }
-  return highlightedJSX;
-}
+
---
+++ sample-app/src/components/utils/renderWithHighlighting.tsx
@@ -0,0 +1,25 @@
+import { AutocompleteResult } from '@yext/answers-core';
+
+/**
+ * Renders an AutocompleteResult with highlighting based on its matchedSubstrings.
+ */
+export default function renderWithHighlighting ({ value, matchedSubstrings }: AutocompleteResult) {
+  if (!matchedSubstrings || matchedSubstrings.length === 0) {
+    return <span>{value}</span>;
+  }
+  const substrings = [...matchedSubstrings];
+  substrings.sort((a, b) => a.offset - b.offset);
+  const highlightedJSX = []
+  let curr = 0;
+  for (let { offset, length } of substrings) {
+    if (offset > curr) {
+      highlightedJSX.push(<span key={curr}>{value.substring(curr, offset)}</span>)
+    }
+    highlightedJSX.push(<strong key={offset}>{value.substring(offset, offset + length)}</strong>)
+    curr = offset + length;
+  }
+  if (curr < value.length) {
+    highlightedJSX.push(<span key={curr}>{value.substring(curr)}</span>)
+  }
+  return highlightedJSX;
+}
