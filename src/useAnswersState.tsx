5d9233c5e714246e0478eec34b027cdeb07545b8
Connor Anderson <canderson@yext.com>
2021-08-09 11:24:25 -0400

    feedback

--- src/useAnswersState.tsx
+++ src/useAnswersState.tsx
@@ -2,28 +2,21 @@ import { useEffect, useState} from 'react';
 import { State } from '@yext/answers-headless/lib/esm/models/state';
 import { useAnswersActions } from './useAnswersActions';
 
-const initialState: State = {
-  query: {},
-  universal: {},
-  vertical: {},
-  filters: {},
-};
-
-type MapState<T> = (s: State) => T;
+type StateMapper<T> = (s: State) => T;
 
 /**
  * Returns the Answers State returned by the map function
  */
-export function useAnswersState<T>(mapState: MapState<T>): T {
-  const [answersState, setState] = useState(initialState);
+export function useAnswersState<T>(mapState: StateMapper<T>): T {
+  const [answersState, setState] = useState({} as T);
   const answersActions = useAnswersActions();
 
   useEffect(() => {
     return answersActions.addListener({
-      valueAccessor: (state: State) => state,
-      callback: (state: State) => setState(state)
+      valueAccessor: mapState,
+      callback: (stateValue: T) => setState(stateValue)
     });
   });
 
-  return mapState(answersState);
+  return answersState;
 }
