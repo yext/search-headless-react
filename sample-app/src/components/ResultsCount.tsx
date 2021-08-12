400ca3d21c31b5b89070c0f1f1f5731559db9661
Christopher Jiang <cjiang@yext.com>
2021-08-12 16:25:43 -0400

    changed request to limit/offset

--- sample-app/src/components/ResultsCount.tsx
+++ sample-app/src/components/ResultsCount.tsx
@@ -3,8 +3,9 @@ import { useAnswersState } from '@yext/answers-headless-react'
 export default function ResultsCount() {
   const vertical = useAnswersState(state => state.vertical);
   const resultsCount = vertical?.results?.verticalResults.resultsCount || 0
-  const resultsLength = vertical?.results?.verticalResults.results.length || 0
-  const offset = vertical?.request?.offset || 0
+  let resultsLength = vertical?.results?.verticalResults.results.length || 0
+
+  const offset = vertical?.offset || 0
 
   const results = ((resultsLength === 0) ? "#Results: none" : `#Results: ${offset + 1}-${offset + resultsLength} of ${resultsCount}`)
 
