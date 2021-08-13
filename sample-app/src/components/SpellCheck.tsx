7e944fe24ed355ac2a5303ff31298e999289f346
Connor Anderson <canderson@yext.com>
2021-08-13 12:22:55 -0400

    update to use button

--- sample-app/src/components/SpellCheck.tsx
+++ sample-app/src/components/SpellCheck.tsx
@@ -1,20 +1,19 @@
-import { useAnswersState } from '@yext/answers-headless-react'
+import { useAnswersState, useAnswersActions } from '@yext/answers-headless-react'
 import '../sass/SpellCheck.scss';
 
 export default function SpellCheck (): JSX.Element | null {
   const correctedQuery = useAnswersState(state => state.spellCheck.correctedQuery);
-  const link = createLink(correctedQuery);
+  const actions = useAnswersActions();
   if (!correctedQuery) {
     return null;
   }
   return (
     <div className='SpellCheck'>
       <span className='SpellCheck__helpText'>Did you mean: </span>
-      <a className='SpellCheck__link' href={link}>{correctedQuery}</a>
+      <button className='SpellCheck__link' onClick={() => {
+        actions.setQuery(correctedQuery);
+        actions.executeVerticalQuery({ skipSpellCheck: true });
+      }}>{correctedQuery}</button>
     </div>
   );
-
-  function createLink (correctedQuery: string = '') : string {
-    return `./?query=${correctedQuery}`;
-  }
 }
