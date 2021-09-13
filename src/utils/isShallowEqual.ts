87e68f0c54865a1dfbe7426ba0ec4600af0d9680
Oliver Shi <oshi@yext.com>
2021-09-13 14:54:42 -0400

    fix for isShallowEqual, add tests
    Fixes a typo in isShallowEqual, which was causing incorrect comparisons
    when the newObj had less keys than the originalObj.
    
    J=SLAP-1579
    TEST=auto, manual
    
    tested using the test scenario provided in https://github.com/yext/answers-headless-react/issues/26

--- src/utils/isShallowEqual.ts
+++ src/utils/isShallowEqual.ts
@@ -4,11 +4,12 @@
  * @returns whether the two objects have values with equal references
  */
 export default function isShallowEqual(
-  originalObj: Record<string, unknown>,
-  newObj: Record<string, unknown>
+  originalObj: Record<string, unknown> | unknown[],
+  newObj: Record<string, unknown> | unknown[]
 ): boolean {
   const newKeys = Object.keys(newObj);
-  if (newKeys.length !== Object.keys(newKeys).length) {
+  const originalKeys = Object.keys(originalObj);
+  if (newKeys.length !== originalKeys.length) {
     return false;
   }
   return newKeys.every(key => {
