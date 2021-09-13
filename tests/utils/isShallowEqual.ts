7c25b15139eb291860c9d6d05824082f12724e10
Oliver Shi <oshi@yext.com>
2021-09-13 15:19:06 -0400

    fix for isShallowEqual, add tests (#27)
    Fixes a typo in isShallowEqual, which was causing incorrect comparisons
    when the newObj had less keys than the originalObj.
    
    J=SLAP-1579
    TEST=auto, manual
    
    tested using the test scenario provided in https://github.com/yext/answers-headless-react/issues/26

---
+++ tests/utils/isShallowEqual.ts
@@ -0,0 +1,80 @@
+import isShallowEqual from '../../src/utils/isShallowEqual';
+
+describe('handles arrays properly', () => {
+  it('equal arrays', () => {
+    const obj = {
+      test: 'hi'
+    };
+    const arr1 = [ 1, 2, obj ];
+    const arr2 = [ 1, 2, obj ];
+    expect(isShallowEqual(arr1, arr2)).toBeTruthy();
+  });
+
+  it('arrays of different sizes', () => {
+    expect(isShallowEqual([], [1])).toBeFalsy();
+    expect(isShallowEqual([1], [])).toBeFalsy();
+  });
+
+  it('arrays with different orderings', () => {
+    const obj = {
+      test: '123'
+    };
+    const arr1 = [ 1, obj ];
+    const arr2 = [ obj, 1 ];
+    expect(isShallowEqual(arr1, arr2)).toBeFalsy();
+  });
+});
+
+it('only compares object value references, rather than doing a deep comparison', () => {
+  const originalObj = {
+    childObj: {
+      test: 'hi'
+    }
+  };
+  const newObj = {
+    childObj: {
+      test: 'hi'
+    }
+  };
+  expect(isShallowEqual(originalObj, newObj)).toBeFalsy();
+});
+
+it('handles objects with equal value references', () => {
+  const childObj = {
+    test: 'hi',
+    more: {
+      nesting: true
+    }
+  };
+  const originalObj = {
+    aNum: 1,
+    aString: 'boo',
+    aBool: true,
+    childObj,
+  };
+  const newObj = {
+    aNum: 1,
+    aString: 'boo',
+    aBool: true,
+    childObj,
+  };
+  expect(isShallowEqual(originalObj, newObj)).toBeTruthy();
+});
+
+it('handles objects with different values', () => {
+  const originalObj = {
+    test: 'hi',
+  };
+  const newObj = {
+    test: 'bye'
+  };
+  expect(isShallowEqual(originalObj, newObj)).toBeFalsy();
+});
+
+it('handles objects with different numbers of keys', () => {
+  const originalObj = {};
+  const newObj = {
+    test: 'bye'
+  };
+  expect(isShallowEqual(originalObj, newObj)).toBeFalsy();
+});
