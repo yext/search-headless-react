66cd0a1a55fed91d193f3943c1826b062ded4fe7
bhaines <bhaines@yext.com>
2021-09-20 18:54:50 -0400

    pr fixes

--- sample-app/src/components/LocationBias.tsx
+++ sample-app/src/components/LocationBias.tsx
@@ -3,75 +3,75 @@ import { LocationBiasMethod } from '@yext/answers-core';
 import { useAnswersActions, useAnswersState } from '@yext/answers-headless-react';
 
 interface Props {
-	isVertical: boolean,
-	geolocationOptions?: {
-		enableHighAccuracy?: boolean
-		timeout?: number
-		maximumAge?: number
-	}
-	cssClasses?: {
-		container: string
-		location: string
-		source: string
-		button: string
-	}
+  isVertical: boolean,
+  geolocationOptions?: {
+    enableHighAccuracy?: boolean
+    timeout?: number
+    maximumAge?: number
+  }
+  cssClasses?: {
+    container: string
+    location: string
+    source: string
+    button: string
+  }
 }
 
 const defaultGeolocationOptions = {
-	enableHighAccuracy: false,
-	timeout: 6000,
-	maximumAge: 300000,
+  enableHighAccuracy: false,
+  timeout: 6000,
+  maximumAge: 300000,
 };
 
 const defaultCSSClasses = {
-		container: 'LocationBias',
-		location: 'LocationBias__location',
-		source: 'LocationBias__source',
-		button: 'LocationBias__button',
+    container: 'LocationBias',
+    location: 'LocationBias__location',
+    source: 'LocationBias__source',
+    button: 'LocationBias__button',
 };
 
 export default function LocationBias(props: Props) {
-	const answersActions = useAnswersActions();
-	const locationBias = useAnswersState(s => s.location.locationBias)
-	const geolocationOptions = Object.assign(defaultGeolocationOptions, props.geolocationOptions);
-	const cssClasses = Object.assign(defaultCSSClasses, props.cssClasses);
+  const answersActions = useAnswersActions();
+  const locationBias = useAnswersState(s => s.location.locationBias)
+  const geolocationOptions = Object.assign(defaultGeolocationOptions, props.geolocationOptions);
+  const cssClasses = Object.assign(defaultCSSClasses, props.cssClasses);
 
-	if (!locationBias || !locationBias?.displayName) return null;
+  if (!locationBias?.displayName) return null;
 
-	const attributionMessage = 
-		  locationBias?.method === LocationBiasMethod.Ip ? '(based on your internet address)'
-		: locationBias?.method === LocationBiasMethod.Device ? '(based on your device)'
-		: '';
+  const attributionMessage = 
+      locationBias?.method === LocationBiasMethod.Ip ? '(based on your internet address)'
+    : locationBias?.method === LocationBiasMethod.Device ? '(based on your device)'
+    : '';
 
-	function handleGeolocationClick() {
-		if ('geolocation' in navigator) {
-			navigator.geolocation.getCurrentPosition((position) => {
-				answersActions.setUserLocation({
-					latitude: position.coords.latitude,
-					longitude: position.coords.longitude,
-				})
-				props.isVertical
-					? answersActions.executeVerticalQuery()
-					: answersActions.executeUniversalQuery()
-			},
-			(err) => console.error(err),
-			geolocationOptions);
-		}
-	}
+  function handleGeolocationClick() {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        answersActions.setUserLocation({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        })
+        props.isVertical
+          ? answersActions.executeVerticalQuery()
+          : answersActions.executeUniversalQuery()
+      },
+      (err) => console.error(err),
+      geolocationOptions);
+    }
+  }
 
-	return (
-		<div className={cssClasses.container}>
-			<span className={cssClasses.location}>
-				{locationBias.displayName}
-			</span>
-			{attributionMessage !== '' && (
-				<span className={cssClasses.source}>
-					{attributionMessage}
-				</span>
-			)}
-			<button className={cssClasses.button} onClick={handleGeolocationClick}>
-				Update your location
-			</button>
-		</div>
-	)
+  return (
+    <div className={cssClasses.container}>
+      <span className={cssClasses.location}>
+        {locationBias.displayName}
+      </span>
+      {attributionMessage !== '' && (
+        <span className={cssClasses.source}>
+          {attributionMessage}
+        </span>
+      )}
+      <button className={cssClasses.button} onClick={handleGeolocationClick}>
+        Update your location
+      </button>
+    </div>
+  )
 }
