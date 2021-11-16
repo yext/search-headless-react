57e3a4afc39d2f02a0d91f71d8ea3fbf2fe81473
Yen Truong <ytruong@yext.com>
2021-11-16 17:49:08 -0500

    rename

--- sample-app/src/hooks/useAutocomplete.tsx
+++ sample-app/src/hooks/useAutocomplete.tsx
@@ -4,14 +4,14 @@ import { AutocompleteResult, useAnswersActions } from '@yext/answers-headless-re
 export function useAutocomplete(isVertical: boolean): [AutocompleteResult[], () => Promise<void>] {
   const answersActions = useAnswersActions();
   const autocompleteNetworkIds = useRef({ latestRequest: 0, responseInState: 0 });
-  const [ autocompleteResults, setAutoCompleteResults ] = useState<AutocompleteResult[]>([]);
+  const [ autocompleteResults, setAutocompleteResults ] = useState<AutocompleteResult[]>([]);
   async function executeAutocomplete () {
     const requestId = ++autocompleteNetworkIds.current.latestRequest;
     const response = isVertical
       ? await answersActions.executeVerticalAutocomplete()
       : await answersActions.executeUniversalAutocomplete();
     if (requestId >= autocompleteNetworkIds.current.responseInState) {
-      setAutoCompleteResults(response?.results || []);
+      setAutocompleteResults(response?.results || []);
       autocompleteNetworkIds.current.responseInState = requestId;
     }
   }
