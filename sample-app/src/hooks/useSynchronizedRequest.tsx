4f21796c080006d0d3c1744cdeeeffc7f78e681f
nmanu1 <nmanu@yext.com>
2021-11-30 16:45:07 -0500

    Rename

--- sample-app/src/hooks/useSynchronizedRequest.tsx
+++ sample-app/src/hooks/useSynchronizedRequest.tsx
@@ -1,15 +1,15 @@
 import { useRef, useState } from "react";
 
-export function useSynchronizedRequest<RequestParams, ResponseType>(
-  executeRequest: (data?: RequestParams) => Promise<ResponseType | undefined>
+export function useSynchronizedRequest<RequestDataType, ResponseType>(
+  executeRequest: (data?: RequestDataType) => Promise<ResponseType | undefined>
 ): [
     ResponseType | undefined,
-    (data?: RequestParams) => Promise<ResponseType | undefined>
+    (data?: RequestDataType) => Promise<ResponseType | undefined>
   ]
 {
   const networkIds = useRef({ latestRequest: 0, responseInState: 0 });
   const [synchronizedResponse, setSynchronizedResponse] = useState<ResponseType>();
-  async function executeSynchronizedRequest (data?: RequestParams): Promise<ResponseType | undefined> {
+  async function executeSynchronizedRequest (data?: RequestDataType): Promise<ResponseType | undefined> {
     const requestId = ++networkIds.current.latestRequest;
     return new Promise(async (resolve) => {
       const response = await executeRequest(data);
