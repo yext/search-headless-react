34584b8d3db1a1c434327abf97c563dbfad4177e
Connor Anderson <canderson@yext.com>
2021-11-30 14:11:22 -0500

    tweaks

--- sample-app/src/components/SearchBar.tsx
+++ sample-app/src/components/SearchBar.tsx
@@ -14,37 +14,37 @@ import { useComposedCssClasses, CompositionMethod } from '../utils/useComposedCs
 
 const SCREENREADER_INSTRUCTIONS = 'When autocomplete results are available, use up and down arrows to review and enter to select.'
 
-interface SearchBarCssClasses {
-  inputDropdownContainer?: string,
-  dropdownContainer?: string,
-  option?: string,
-  inputElement?: string,
-  inputContainer?: string,
-  logoContainer?: string,
-  searchButtonContainer?: string,
-  submitButton?: string,
-  container?: string,
-  divider?: string,
-  resultIconContainer?: string,
-  sectionContainer?: string,
-  sectionLabel?: string,
-  optionsContainer?: string,
-  focusedOption?: string
-}
-
 const builtInCssClasses: SearchBarCssClasses = {
   container: 'h-12',
-  inputDropdownContainer: 'bg-white shadow border rounded-3xl border-gray-300 w-full overflow-hidden',
+  divider: 'border mx-2',
+  dropdownContainer: 'relative bg-white py-1',
+  focusedOption: 'bg-gray-100',
   inputContainer: 'h-12 inline-flex items-center justify-between w-full',
+  inputDropdownContainer: 'bg-white shadow border rounded-3xl border-gray-300 w-full overflow-hidden',
   inputElement: 'outline-none flex-grow border-none h-full px-2',
   logoContainer: 'w-8 mx-2',
-  searchButtonContainer: 'w-8 h-full mx-2',
-  submitButton: 'h-full w-full',
-  dropdownContainer: 'relative bg-white py-1',
   option: 'flex items-center py-1 px-2 cursor-pointer',
-  focusedOption: 'bg-gray-100',
-  divider: 'border mx-2',
-  resultIconContainer: 'opacity-20 w-8 h-8 pl-1 mr-4'
+  resultIconContainer: 'opacity-20 w-8 h-8 pl-1 mr-4',
+  searchButtonContainer: 'w-8 h-full mx-2',
+  submitButton: 'h-full w-full'
+}
+
+interface SearchBarCssClasses {
+  container?: string,
+  divider?: string,
+  dropdownContainer?: string,
+  focusedOption?: string,
+  inputContainer?: string,
+  inputDropdownContainer?: string,
+  inputElement?: string,
+  logoContainer?: string,
+  option?: string,
+  optionsContainer?: string,
+  resultIconContainer?: string,
+  searchButtonContainer?: string,
+  sectionContainer?: string,
+  sectionLabel?: string,
+  submitButton?: string
 }
 
 interface Props {
@@ -52,7 +52,7 @@ interface Props {
   isVertical: boolean,
   geolocationOptions?: PositionOptions,
   screenReaderInstructionsId: string,
-  cssClasses?: SearchBarCssClasses,
+  customCssClasses?: SearchBarCssClasses,
   cssCompositionMethod?: CompositionMethod
 }
 
@@ -64,7 +64,7 @@ export default function SearchBar({
   isVertical,
   geolocationOptions,
   screenReaderInstructionsId,
-  cssClasses: customCssClasses,
+  customCssClasses,
   cssCompositionMethod
 }: Props) {
   const cssClasses = useComposedCssClasses(builtInCssClasses, customCssClasses, cssCompositionMethod);
