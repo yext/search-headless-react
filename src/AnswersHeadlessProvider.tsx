ca5989537116557a1d1682e572162be982be47d7
Yen Truong <ytruong@yext.com>
2021-11-30 14:01:03 -0500

    SessionId support
    - update AnswersHeadlessProvider to accept a sessionTrackingEnabled field. If true, generate a uuid and set that as sessionId.
    
    J=SLAP-1726
    TEST=manual
    
    - clear all cookies, set sessionTrackingEnabled to true. See that sessionId is tagged in universal and vertical search request. set sessionTrackingEnabled to false and see that sessionId no longer appears
    - set sessionTrackingEnabled to true and commented out setSessionId. see that cookie is dropped.

--- src/AnswersHeadlessProvider.tsx
+++ src/AnswersHeadlessProvider.tsx
@@ -1,16 +1,22 @@
 import { ReactChild, ReactChildren } from 'react';
 import { provideAnswersHeadless, AnswersHeadless, AnswersConfig } from '@yext/answers-headless';
 import { AnswersHeadlessContext } from './AnswersHeadlessContext';
+import { v4 as uuidv4 } from 'uuid';
 
-interface Props extends AnswersConfig {
+type Props = AnswersConfig & {
   children?: ReactChildren | ReactChild | (ReactChildren | ReactChild)[],
-  verticalKey?: string
-}
+  verticalKey?: string,
+  sessionTrackingEnabled?: boolean
+};
 
 export function AnswersHeadlessProvider(props: Props): JSX.Element {
-  const { children, verticalKey, ...answersConfig } = props;
+  const { children, verticalKey, sessionTrackingEnabled=true, ...answersConfig } = props;
   const answers: AnswersHeadless = provideAnswersHeadless(answersConfig);
   verticalKey && answers.setVerticalKey(verticalKey);
+  answers.setSessionTrackingEnabled(sessionTrackingEnabled);
+  if (sessionTrackingEnabled && !answers.state.sessionTracking.sessionId) {
+    answers.setSessionId(uuidv4());
+  }
   return (
     <AnswersHeadlessContext.Provider value={answers}>
       {children}
