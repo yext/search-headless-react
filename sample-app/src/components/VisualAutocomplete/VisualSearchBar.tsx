94f42797566ecd6f39459af700672d4a3c7bee23
Yen Truong <ytruong@yext.com>
2021-12-08 12:58:20 -0500

    bug

--- sample-app/src/components/VisualAutocomplete/VisualSearchBar.tsx
+++ sample-app/src/components/VisualAutocomplete/VisualSearchBar.tsx
@@ -180,6 +180,9 @@ export default function VisualSearchBar({
     return <DropdownSection
       options={options}
       optionIdPrefix='VisualSearchBar-RecentSearches'
+      onFocusChange={value => {
+        answersActions.setQuery(value);
+      }}
       onSelectOption={optionValue => {
         autocompletePromiseRef.current = undefined;
         answersActions.setQuery(optionValue);
