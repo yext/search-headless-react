d07074d8f95dd8cd2e1e4cf1abd125c2d874e36b
Connor Anderson <canderson@yext.com>
2021-12-08 10:19:16 -0500

    tweaks

--- sample-app/src/components/cards/StandardCard.tsx
+++ sample-app/src/components/cards/StandardCard.tsx
@@ -29,8 +29,8 @@ const builtInCssClasses: StandardCardCssClasses = {
   body: 'flex justify-end pt-2.5',
   descriptionContainer: 'w-full text-base',
   ctaContainer: 'flex flex-col justify-end ml-4',
-  cta1: 'min-w-max bg-blue-600 text-white font-medium rounded-lg py-2 px-5',
-  cta2: 'min-w-max bg-white text-blue-600 font-medium rounded-lg py-2 px-5 mt-2 border',
+  cta1: 'min-w-max bg-blue-600 text-white font-medium rounded-lg py-2 px-5 shadow',
+  cta2: 'min-w-max bg-white text-blue-600 font-medium rounded-lg py-2 px-5 mt-2 shadow',
   ordinal: 'mr-1.5 text-lg font-medium',
   title: 'text-lg font-medium'
 }
