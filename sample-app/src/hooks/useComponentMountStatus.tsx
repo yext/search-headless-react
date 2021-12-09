7aa4889673e469df606d6b9d0df3c0407687468b
Yen Truong <ytruong@yext.com>
2021-12-09 17:09:15 -0500

    comment

--- sample-app/src/hooks/useComponentMountStatus.tsx
+++ sample-app/src/hooks/useComponentMountStatus.tsx
@@ -1,5 +1,11 @@
 import { useEffect, useRef } from "react";
 
+/**
+ * This is use to prevent react errors due to performing state update on unmounted component.
+ * This error does not need to be suppressed as it will be remove in the next version of React.
+ * 
+ * React PR regarding the issue: https://github.com/facebook/react/pull/22114
+ */
 export default function useComponentMountStatus() {
   const isMountedRef = useRef<boolean>(false);
   useEffect(() => {
