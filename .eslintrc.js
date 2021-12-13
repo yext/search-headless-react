64bb77ad032525f36ee6f937002ba0bc9a6ac00b
Yen Truong <ytruong@yext.com>
2021-12-13 15:33:40 -0500

    remove script from eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
     'plugin:@yext/export-star/recommended'
   ],
   plugins: ['react-perf'],
-  ignorePatterns: ['lib', 'tests/setup/responses', 'scripts'],
+  ignorePatterns: ['lib', 'tests/setup/responses'],
   rules: {
     indent: ['error', 2, { SwitchCase: 1 }],
     'no-trailing-spaces': ['error'],
