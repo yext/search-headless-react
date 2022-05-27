25826c9b8c7dff33d3114066169b72682379d231
Oliver Shi <oshi@yext.com>
2022-05-27 14:30:39 -0400

    export SandboxEndpoints
    Updates answers-headless to the next alpha version which now exports a
    SandboxEndpoints object.
    
    J=SLAP-2106
    TEST=auto
    
    added test for checking that an answers config is passed from AnswersHeadlessProvider
    to provideAnswersHeadless without the endpoints being altered

---
+++ tests/AnswersHeadlessProvider.test.tsx
@@ -0,0 +1,23 @@
+import { AnswersHeadlessProvider, SandboxEndpoints } from '../src';
+import { render } from '@testing-library/react';
+import { provideAnswersHeadless } from '@yext/answers-headless';
+
+jest.mock('@yext/answers-headless', () => ({
+  provideAnswersHeadless: jest.fn(() => ({
+    setSessionTrackingEnabled: jest.fn(),
+    setSessionId: jest.fn()
+  }))
+}));
+
+it('correctly passes through an answers config with sandbox endpoints', () => {
+  const config = {
+    apiKey: '<apiKey>',
+    experienceKey: '<experienceKey>',
+    locale: 'en',
+    endpoints: SandboxEndpoints
+  };
+
+  render(<AnswersHeadlessProvider {...config}/>);
+  expect(provideAnswersHeadless).toHaveBeenCalledTimes(1);
+  expect(provideAnswersHeadless).toHaveBeenCalledWith(config, expect.anything());
+});
