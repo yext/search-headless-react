f14e8c9086b22c3b711f5416e071a86e868fe1ba
Cindy Yang <cyang@yext.com>
2022-07-05 11:13:12 -0400

    Deprecation fix

--- src/index.ts
+++ src/index.ts
@@ -19,14 +19,36 @@ export {
 };
 
 /**
- * @public @deprecated Answers components should be replaced with their Search counterparts
+ * @deprecated AnswersHeadlessContext has been deprecated and replaced by SearchHeadlessContext
  */
-export {
-  useSearchActions as useAnswersActions,
-  SearchActions as AnswersActions,
-  useSearchState as useAnswersState,
-  useSearchUtilities as useAnswersUtilities,
-  SearchUtilities as AnswersUtilities,
-  SearchHeadlessProvider as AnswersHeadlessProvider,
-  SearchHeadlessContext as AnswersHeadlessContext
-};
+export const AnswersHeadlessContext = SearchHeadlessContext;
+
+/**
+ * @deprecated AnswersActions has been deprecated and replaced by SearchActions
+ */
+export type AnswersActions = SearchActions;
+
+/**
+ * @deprecated AnswersUtilities has been deprecated and replaced by SearchUtilities
+ */
+export type AnswersUtilities = SearchUtilities;
+
+/**
+ * @deprecated useAnswersActions has been deprecated and replaced by useSearchActions
+ */
+export const useAnswersActions = useSearchActions;
+
+/**
+ * @deprecated useAnswersState has been deprecated and replaced by useSearchState
+ */
+export const useAnswersState = useSearchState;
+
+/**
+ * @deprecated useAnswersUtilities has been deprecated and repalced by useSearchUtilities
+ */
+export const useAnswersUtilities = useSearchUtilities;
+
+/**
+ * @deprecated AnswersHeadlessProvider has been deprecated and replaced by SearfchHeadlessProvider
+ */
+export const AnswersHeadlessProvider = SearchHeadlessProvider;
