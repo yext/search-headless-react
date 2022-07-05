2af6e363bc2bcca3e2f14640257bef9cd4727ba4
Cindy <74145775+ElemelonWind@users.noreply.github.com>
2022-07-05 14:05:29 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -18,37 +18,23 @@ export {
   StateSelector
 };
 
-/**
- * @deprecated AnswersHeadlessContext has been deprecated and replaced by SearchHeadlessContext
- */
+/** @deprecated AnswersHeadlessContext has been deprecated and replaced by SearchHeadlessContext */
 export const AnswersHeadlessContext = SearchHeadlessContext;
 
-/**
- * @deprecated AnswersActions has been deprecated and replaced by SearchActions
- */
+/** @deprecated AnswersActions has been deprecated and replaced by SearchActions */
 export type AnswersActions = SearchActions;
 
-/**
- * @deprecated AnswersUtilities has been deprecated and replaced by SearchUtilities
- */
+/** @deprecated AnswersUtilities has been deprecated and replaced by SearchUtilities */
 export type AnswersUtilities = SearchUtilities;
 
-/**
- * @deprecated useAnswersActions has been deprecated and replaced by useSearchActions
- */
+/** @deprecated useAnswersActions has been deprecated and replaced by useSearchActions */
 export const useAnswersActions = useSearchActions;
 
-/**
- * @deprecated useAnswersState has been deprecated and replaced by useSearchState
- */
+/** @deprecated useAnswersState has been deprecated and replaced by useSearchState */
 export const useAnswersState = useSearchState;
 
-/**
- * @deprecated useAnswersUtilities has been deprecated and repalced by useSearchUtilities
- */
+/** @deprecated useAnswersUtilities has been deprecated and repalced by useSearchUtilities */
 export const useAnswersUtilities = useSearchUtilities;
 
-/**
- * @deprecated AnswersHeadlessProvider has been deprecated and replaced by SearfchHeadlessProvider
- */
+/** @deprecated AnswersHeadlessProvider has been deprecated and replaced by SearfchHeadlessProvider */
 export const AnswersHeadlessProvider = SearchHeadlessProvider;
