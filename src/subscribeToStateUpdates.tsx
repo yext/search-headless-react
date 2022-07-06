98ae1d51a9cebdbb1c9fc0b6752f13383159ed8b
Yen Truong <ytruong@yext.com>
2022-07-06 12:59:13 -0400

    deprecate subscribeToStateUpdates, update README

--- src/subscribeToStateUpdates.tsx
+++ src/subscribeToStateUpdates.tsx
@@ -1,7 +1,3 @@
-// TODO(SLAP-1485): find out how to specify generic component props without using `any`
-// I sank a 3-4 hours into this but couldn't figure out exactly how to get it to work.
-// May require use of typescript generics.
-
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { ComponentType, useReducer, useEffect, useContext } from 'react';
 import { State } from '@yext/answers-headless';
@@ -11,6 +7,10 @@ import isShallowEqual from './utils/isShallowEqual';
 type SubscriberGenerator = (WrappedComponent: ComponentType<any>) => (props: any) => JSX.Element;
 
 /**
+ * @deprecated
+ * For class component, use `AnswersHeadlessContext` directly to dispatch actions and receive state updates.
+ * For functional component, use `useAnswersAcions` and `useAnswersState` instead.
+ *
  * Generates a HOC that updates a given Component's props based on the current
  * answers-headless state and a given mapping function.
  */
