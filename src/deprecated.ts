49bf6a655f03307e872cf7eff1c1639d3550aa34
Cindy Yang <cyang@yext.com>
2022-07-12 16:06:52 -0400

    fix lint issues

--- src/deprecated.ts
+++ src/deprecated.ts
@@ -7,34 +7,34 @@ import { SearchHeadlessContext } from './SearchHeadlessContext';
 /**
  * @deprecated AnswersHeadlessContext has been deprecated and replaced by SearchHeadlessContext
  */
- export const AnswersHeadlessContext = SearchHeadlessContext;
+export const AnswersHeadlessContext = SearchHeadlessContext;
 
- /**
-  * @deprecated AnswersActions has been deprecated and replaced by SearchActions
-  */
- export type AnswersActions = SearchActions;
- 
- /**
-  * @deprecated AnswersUtilities has been deprecated and replaced by SearchUtilities
-  */
- export type AnswersUtilities = SearchUtilities;
- 
- /**
-  * @deprecated useAnswersActions has been deprecated and replaced by useSearchActions
-  */
- export const useAnswersActions = useSearchActions;
- 
- /**
-  * @deprecated useAnswersState has been deprecated and replaced by useSearchState
-  */
- export const useAnswersState = useSearchState;
- 
- /**
-  * @deprecated useAnswersUtilities has been deprecated and repalced by useSearchUtilities
-  */
- export const useAnswersUtilities = useSearchUtilities;
- 
- /**
-  * @deprecated AnswersHeadlessProvider has been deprecated and replaced by SearfchHeadlessProvider
-  */
- export const AnswersHeadlessProvider = SearchHeadlessProvider;
+/**
+ * @deprecated AnswersActions has been deprecated and replaced by SearchActions
+ */
+export type AnswersActions = SearchActions;
+
+/**
+ * @deprecated AnswersUtilities has been deprecated and replaced by SearchUtilities
+ */
+export type AnswersUtilities = SearchUtilities;
+
+/**
+ * @deprecated useAnswersActions has been deprecated and replaced by useSearchActions
+ */
+export const useAnswersActions = useSearchActions;
+
+/**
+ * @deprecated useAnswersState has been deprecated and replaced by useSearchState
+ */
+export const useAnswersState = useSearchState;
+
+/**
+ * @deprecated useAnswersUtilities has been deprecated and repalced by useSearchUtilities
+ */
+export const useAnswersUtilities = useSearchUtilities;
+
+/**
+ * @deprecated AnswersHeadlessProvider has been deprecated and replaced by SearfchHeadlessProvider
+ */
+export const AnswersHeadlessProvider = SearchHeadlessProvider;
