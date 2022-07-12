9c1bc22d9514db66ebda5425a532b0fcebcbd4f1
Cindy Yang <cyang@yext.com>
2022-07-12 12:49:44 -0400

    deprecated file

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import { SearchHeadlessProvider } from './SearchHeadlessProvider';
 import { SearchHeadlessContext } from './SearchHeadlessContext';
 
 export * from '@yext/answers-headless';
+export * from './deprecated';
 export {
   SearchHeadlessContext,
   subscribeToStateUpdates,
@@ -17,38 +18,3 @@ export {
   SearchUtilities,
   StateSelector
 };
-
-/**
- * @deprecated AnswersHeadlessContext has been deprecated and replaced by SearchHeadlessContext
- */
-export const AnswersHeadlessContext = SearchHeadlessContext;
-
-/**
- * @deprecated AnswersActions has been deprecated and replaced by SearchActions
- */
-export type AnswersActions = SearchActions;
-
-/**
- * @deprecated AnswersUtilities has been deprecated and replaced by SearchUtilities
- */
-export type AnswersUtilities = SearchUtilities;
-
-/**
- * @deprecated useAnswersActions has been deprecated and replaced by useSearchActions
- */
-export const useAnswersActions = useSearchActions;
-
-/**
- * @deprecated useAnswersState has been deprecated and replaced by useSearchState
- */
-export const useAnswersState = useSearchState;
-
-/**
- * @deprecated useAnswersUtilities has been deprecated and repalced by useSearchUtilities
- */
-export const useAnswersUtilities = useSearchUtilities;
-
-/**
- * @deprecated AnswersHeadlessProvider has been deprecated and replaced by SearfchHeadlessProvider
- */
-export const AnswersHeadlessProvider = SearchHeadlessProvider;
